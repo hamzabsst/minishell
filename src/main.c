678c07a6a8124f60b86b98acf7b657111ad37e60
Hamza Boussetta <hbousset@c2r9p5.1337.ma>
2025-04-22 10:30:27 +0100

    fixed norminette, almost finished pipex

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: hbousset <hbousset@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/19 21:59:49 by hbousset          #+#    #+#             */
-/*   Updated: 2025/04/22 10:11:25 by hbousset         ###   ########.fr       */
+/*   Updated: 2025/04/22 10:13:51 by hbousset         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,14 +23,14 @@ int	main(int ac, char **av, char **env)
 		.append = 0,
 		.next = NULL
 	};
- 	t_cmd c2 = {
+ 	t_cmd cmd1 = {
 		.av = (char *[]){"wc", NULL},
 		.infile = NULL,
 		.outfile = "result.txt",
 		.append = 0,
 		.next = NULL
 	};
-	cmd.next = &c2;
+	cmd.next = &cmd1;
 	(void)av;
 	if (ac != 1)
 	{
@@ -38,7 +38,6 @@ int	main(int ac, char **av, char **env)
 		exit(1);
 	}
 	g_env = dup_env(env);
-
 	if (!cmd.next && builtin(cmd.av[0]))
 		return exec_builtin(&cmd, &g_env);
 	else
