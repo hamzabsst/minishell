f367cc875adc4a95dd66fe7111f17d3e1760dae1
Hamza Boussetta <hbousset@c2r1p5.1337.ma>
2025-04-23 10:13:45 +0100

    drt parsing dyal 3bar hhhhhhhhhhh

--- src/main.c
+++ src/main.c
@@ -6,34 +6,198 @@
 /*   By: hbousset <hbousset@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/19 21:59:49 by hbousset          #+#    #+#             */
-/*   Updated: 2025/04/22 11:30:29 by hbousset         ###   ########.fr       */
+/*   Updated: 2025/04/23 10:13:16 by hbousset         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+t_cmd	*create_cmd(char **args)
+{
+	t_cmd *cmd = malloc(sizeof(t_cmd));
+	if (!cmd)
+		return (NULL);
+	cmd->av = args;
+	cmd->infile = NULL;
+	cmd->outfile = NULL;
+	cmd->append = 0;
+	cmd->heredoc = 0;
+	cmd->delimiter = NULL;
+	cmd->next = NULL;
+	return (cmd);
+}
+
+void	free_cmd_list(t_cmd *cmd)
+{
+	while (cmd)
+	{
+		t_cmd *next = cmd->next;
+		ft_free(cmd->av);
+		if (cmd->infile)
+			free(cmd->infile);
+		if (cmd->outfile)
+			free(cmd->outfile);
+		if (cmd->delimiter)
+			free(cmd->delimiter);
+		free(cmd);
+		cmd = next;
+	}
+}
+
+void handle_redirections(t_cmd *cmd)
+{
+    int i = 0;
+    int j;
+
+    while (cmd->av[i])
+    {
+        if (ft_strcmp(cmd->av[i], "<") == 0 && cmd->av[i + 1])
+        {
+            cmd->infile = ft_strdup(cmd->av[i + 1]);
+            j = i;
+            while (cmd->av[j + 2])
+            {
+                cmd->av[j] = cmd->av[j + 2];
+                j++;
+            }
+            cmd->av[j] = NULL;
+            continue;
+        }
+        else if (ft_strcmp(cmd->av[i], ">") == 0 && cmd->av[i + 1])
+        {
+            cmd->outfile = ft_strdup(cmd->av[i + 1]);
+            cmd->append = 0;
+            j = i;
+            while (cmd->av[j + 2])
+            {
+                cmd->av[j] = cmd->av[j + 2];
+                j++;
+            }
+            cmd->av[j] = NULL;
+            continue;
+        }
+        else if (ft_strcmp(cmd->av[i], ">>") == 0 && cmd->av[i + 1])
+        {
+            cmd->outfile = ft_strdup(cmd->av[i + 1]);
+            cmd->append = 1;
+            j = i;
+            while (cmd->av[j + 2])
+            {
+                cmd->av[j] = cmd->av[j + 2];
+                j++;
+            }
+            cmd->av[j] = NULL;
+            continue;
+        }
+        else if (ft_strcmp(cmd->av[i], "<<") == 0 && cmd->av[i + 1])
+        {
+            cmd->heredoc = 1;
+            cmd->delimiter = ft_strdup(cmd->av[i + 1]);
+            j = i;
+            while (cmd->av[j + 2])
+            {
+                cmd->av[j] = cmd->av[j + 2];
+                j++;
+            }
+            cmd->av[j] = NULL;
+            continue;
+        }
+        i++;
+    }
+}
+
+t_cmd	*mock_parse(char *input)
+{
+	t_cmd *head = NULL, *last = NULL;
+	char **pipe_parts = ft_split(input, '|');
+	if (!pipe_parts)
+		return (NULL);
+
+	for (int i = 0; pipe_parts[i]; i++)
+	{
+		char *trimmed = ft_strtrim(pipe_parts[i], " \t\n");
+		if (!trimmed)
+        {
+            ft_free(pipe_parts);
+            return (head);
+        }
+		if (*trimmed == '\0')
+        {
+            free(trimmed);
+            continue;
+		}
+		char **args = ft_split(trimmed, ' ');
+		free(trimmed);
+		if (!args || !args[0])
+        {
+            if (args)
+                ft_free(args);
+			continue;
+        }
+		t_cmd *new_cmd = create_cmd(args);
+		if (!new_cmd)
+        {
+            ft_free(args);
+            ft_free(pipe_parts);
+            free_cmd_list(head);
+            return (NULL);
+        }
+        handle_redirections(new_cmd);
+		if (!head)
+			head = new_cmd;
+		else
+			last->next = new_cmd;
+		last = new_cmd;
+	}
+	ft_free(pipe_parts);
+	return (head);
+}
+
 int	main(int ac, char **av, char **env)
 {
-	char	**g_env;
+	char	*line;
+	char	**g_env = dup_env(env);
+	int 	g_exit_status = 0;
 
-	t_cmd cmd = {
-		.av = (char *[]){"cat", NULL},
-		.infile = "Makefile",
-		.outfile = "out.txt",
-		.append = 0,
-		.heredoc = 0,
-		.delimiter = NULL,
-		.next = NULL
-	};
-	(void)av;
 	if (ac != 1)
 	{
-		write(2, "Error: Invalid number of arguments\n", 35);
+		write(2, "Invalid number of arguments\n", 29);
+		ft_free(g_env);
 		exit(1);
 	}
-	g_env = dup_env(env);
-	if (!cmd.next && builtin(cmd.av[0]))
-		return exec_builtin(&cmd, &g_env);
-	else
-		exec_pipeline(&cmd, g_env);
+	(void)av;
+
+	while (1)
+	{
+		line = readline("minishell$ ");
+		if (!line)
+		{
+			write(1, "exit\n", 5);
+			break;
+		}
+		if (*line)
+			add_history(line);
+		t_cmd *cmd = mock_parse(line);
+		if (cmd)
+		{
+			if (builtin(cmd->av[0]))
+			{
+				g_exit_status = exec_builtin(cmd, &g_env);
+			}
+			else
+			{
+				g_exit_status = exec_pipeline(cmd, g_env);
+			}
+			free_cmd_list(cmd);
+		}
+		else if (*line)
+		{
+			printf("Parse error.\n");
+			g_exit_status = 1;
+		}
+
+		free(line);
+	}
+	ft_free(g_env);
+	return (g_exit_status);
 }
--- src/pipe/redirection.c
+++ src/pipe/redirection.c
@@ -6,28 +6,44 @@
 /*   By: hbousset <hbousset@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/22 11:26:00 by hbousset          #+#    #+#             */
-/*   Updated: 2025/04/22 11:32:41 by hbousset         ###   ########.fr       */
+/*   Updated: 2025/04/23 09:31:54 by hbousset         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static void	in_from_heredoc(void)
+static void	in_from_heredoc(t_cmd *cmd)
 {
-	int	fd;
+	int		tmp_fd;
+	char	*line;
+	char	*joined;
 
-	fd = open(".heredoc_tmp", O_RDONLY);
-	if (fd == -1)
+	unlink("/tmp/.heredoc_tmp");
+	tmp_fd = open("/tmp/.here_doc_tmp", O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	if (tmp_fd < 0)
+		(ft_putstr_fd("cannot open tmp file", 2), exit(EXIT_FAILURE));
+	joined = ft_strjoin(cmd->delimiter, "\n");
+	if (!joined)
+		(close(tmp_fd), unlink("/tmp/.here_doc_tmp"), exit(EXIT_FAILURE));
+	while (1)
 	{
-		perror("heredoc open");
-		exit(1);
+		write(1, "heredoc> ", 9);
+		line = get_next_line(STDIN_FILENO);
+		if (!line || !ft_strcmp(line, cmd->delimiter) || !ft_strcmp(line, joined))
+		{
+			free(line);
+			break ;
+		}
+		(ft_putstr_fd(line, tmp_fd), free(line));
 	}
-	if (dup2(fd, STDIN_FILENO) == -1)
-	{
-		perror("dup2 heredoc");
-		exit(1);
-	}
-	close(fd);
+	get_next_line(-1);
+	(free(joined), close(tmp_fd));
+	tmp_fd = open("/tmp/.heredoc_tmp", O_RDONLY);
+	if (tmp_fd < 0)
+		(ft_putstr_fd("cannot open tmp file", 2), exit(EXIT_FAILURE));
+	if (dup2(tmp_fd, STDIN_FILENO) == -1)
+		(perror("dup2 heredoc"), exit(EXIT_FAILURE));
+	close(tmp_fd);
 }
 
 static void	in_from_file(char *infile)
@@ -75,7 +91,7 @@ static void	out_to_file(char *outfile, int append)
 void	handle_redirection(t_cmd *cmd)
 {
 	if (cmd->heredoc)
-		in_from_heredoc();
+		in_from_heredoc(cmd);
 	else if (cmd->infile)
 		in_from_file(cmd->infile);
 	if (cmd->outfile)
