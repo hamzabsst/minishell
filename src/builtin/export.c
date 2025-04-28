860373fcf8f8dfc3bf38f1f16de1b10c0025d097
Hamza Boussetta <hbousset@c2r1p5.1337.ma>
2025-04-28 09:29:33 +0100

    export

--- src/builtin/export.c
+++ src/builtin/export.c
@@ -6,12 +6,52 @@
 /*   By: hbousset <hbousset@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/21 09:59:49 by hbousset          #+#    #+#             */
-/*   Updated: 2025/04/23 11:57:19 by hbousset         ###   ########.fr       */
+/*   Updated: 2025/04/26 18:54:54 by hbousset         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-//dont forget to handle arguments
+//dont forget to handle avuments
+// export VAR=value     ✅
+// export VAR           ✅ (sets empty string)
+// export VAR+=value    ✅ (append if exists)
+// export 1VAR=xx       ❌ (invalid)
+// export =             ❌ (invalid)
+
+static int	is_valid(char *s)
+{
+	int	i;
+
+	i = 0;
+	if (!s || !(ft_isalpha(s[i]) || s[i] == '_'))
+		return (0);
+	i++;
+	while (s[i] && s[i] != '=')
+	{
+		if (!(ft_isalnum(s[i]) || s[i] == '_'))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+static void	split_export(char *av, char **key, char **value)
+{
+	char	*equal;
+
+	equal = ft_strchr(av, '=');
+	if (!equal)
+	{
+		*key = ft_strdup(av);
+		*value = NULL;
+	}
+	else
+	{
+		*key = ft_substr(av, 0, equal - av);
+		*value = ft_strdup(equal + 1);
+	}
+}
+
 static void	sort_env(char **env)
 {
 	int		i;
@@ -36,7 +76,7 @@ static void	sort_env(char **env)
 	}
 }
 
-static void	print_export(char **env)
+static int	print_export(char **env)
 {
 	int		i;
 	char	**copy;
@@ -45,7 +85,7 @@ static void	print_export(char **env)
 	i = 0;
 	copy = dup_env(env);
 	if (!copy)
-		return ;
+		return (1);
 	sort_env(copy);
 	while (copy[i])
 	{
@@ -61,15 +101,38 @@ static void	print_export(char **env)
 		i++;
 	}
 	ft_free(copy);
+	return (0);
 }
 
 int	builtin_export(char **av, char **env)
 {
-	if (av[1])
+	int		i;
+	char	*key;
+	char	*value;
+
+	i = 0;
+	if (!av[1])
+		return (print_export(env));
+	while (av[i])
 	{
-		write(2, "export: arguments not supported yet\n", 36);
-		return (1);
+		if (!is_valid(av[i]))
+		{
+			ft_putstr_fd("export: `", 2);
+			ft_putstr_fd(av[i], 2);
+			ft_putstr_fd("': not a valid identifier\n", 2);
+			i++;
+			continue;
+		}
+		split_export(av[i], &key, &value);
+		if (!key)
+			return (1);
+		if (value)
+			update_env(env, key, value);
+		else if (!get_env_value(env, key))
+			update_env(env, key, "");
+		free(key);
+		free(value);
+		i++;
 	}
-	print_export(env);
 	return (0);
 }
