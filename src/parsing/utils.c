d0acbb3f2a0fe8f89e825da0165c95b77b71ab6d
abderrahman22 <abderrahmanchaman21@gmail.com>
2025-05-29 10:11:29 +0100

    still fixing quoting

--- src/parsing/utils.c
+++ src/parsing/utils.c
@@ -6,7 +6,7 @@
 /*   By: abchaman <abchaman@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/28 10:51:14 by abchaman          #+#    #+#             */
-/*   Updated: 2025/05/28 18:05:53 by abchaman         ###   ########.fr       */
+/*   Updated: 2025/05/29 10:10:03 by abchaman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -154,7 +154,7 @@ char	**smart_split(t_cmd *cmd, char *str)
 				{
 					while (str[i + 1] == '\'' || str[i + 1] == '\"' || str[i + 1] == '>' || str[i + 1] == '<')
 					{
-						quote_type = str[i];
+						quote_type = str[i + 1];
 						i++;
 					}
 					i++;
