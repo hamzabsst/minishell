ae72b1c6aa9ceeda67b1bf822198e1b881242754
Hamza Boussetta <hbousset@c2r2p3.1337.ma>
2025-06-30 15:07:46 +0100

    fixed exit code with no args , still have a problem in ctrl c in heredoc

--- src/execution/redirection/heredoc.c
+++ src/execution/redirection/heredoc.c
@@ -6,7 +6,7 @@
 /*   By: hbousset <hbousset@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/20 09:25:52 by hbousset          #+#    #+#             */
-/*   Updated: 2025/06/30 15:00:16 by hbousset         ###   ########.fr       */
+/*   Updated: 2025/06/30 15:07:38 by hbousset         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,18 @@ void	handle_heredoc_sigint(int sig)
 	g_var = 2;
 	ft_putstr_fd("\n", 1);
 	close(STDIN_FILENO);
+}
+
+void	clean_heredoc(int fd, const char *path, int in, void (*handler)(int))
+{
+	if (fd >= 0)
+		close(fd);
+	if (path)
+		unlink(path);
+	if (in >= 0)
+		restore_io(in, -1);
+	if (handler)
+		signal(SIGINT, handler);
 }
 
 static int	setup_heredoc(t_cmd *cmd, char *filepath, int *fd)
@@ -40,22 +52,6 @@ static int	setup_heredoc(t_cmd *cmd, char *filepath, int *fd)
 	return (stdin_backup);
 }
 
-// Fixed clean_heredoc - DON'T reset g_var!
-void	clean_heredoc(int fd, const char *path, int in, void (*handler)(int))
-{
-	if (fd >= 0)
-		close(fd);
-	if (path)
-		unlink(path);
-	if (in >= 0)
-		restore_io(in, -1);
-	if (handler)
-		signal(SIGINT, handler);
-	// DON'T reset g_var here! Let the main loop handle it
-	// g_var = 0;  // ← REMOVE THIS LINE
-}
-
-// Fixed read_heredoc function
 static int	read_heredoc(int fd, char *clean_delim, int in_backup, char *path)
 {
 	char	*line;
@@ -65,39 +61,11 @@ static int	read_heredoc(int fd, char *clean_delim, int in_backup, char *path)
 	while (1)
 	{
 		if (g_var == 2)
-		{
-			if (fd >= 0)
-				close(fd);
-			if (path)
-				unlink(path);
-			if (in_backup >= 0)
-				restore_io(in_backup, -1);
-			if (handler)
-				signal(SIGINT, handler);
-			// DON'T reset g_var here!
-			return (-1);
-		}
-
-		write(STDOUT_FILENO, "> ", 2);
-		line = get_next_line(0);
-
+			return (clean_heredoc(fd, path, in_backup, handler), -1);
+		(write(STDOUT_FILENO, "> ", 2), line = get_next_line(0));
 		if (g_var == 2)
-		{
-			get_next_line(-1);
-			free(line);
-			// Clean up but preserve g_var
-			if (fd >= 0)
-				close(fd);
-			if (path)
-				unlink(path);
-			if (in_backup >= 0)
-				restore_io(in_backup, -1);
-			if (handler)
-				signal(SIGINT, handler);
-			// DON'T reset g_var here!
-			return (-1);
-		}
-
+			return (get_next_line(-1), free(line),
+				clean_heredoc(fd, path, in_backup, handler), -1);
 		if (!line)
 		{
 			our_perror("\nwarning: here-document delimited by end-of-file\n");
@@ -105,18 +73,12 @@ static int	read_heredoc(int fd, char *clean_delim, int in_backup, char *path)
 		}
 		if (compare_delimiter(line, clean_delim))
 		{
-			get_next_line(-1);
-			free(line);
+			(get_next_line(-1), free(line));
 			break ;
 		}
-		write(fd, line, ft_strlen(line));
-		free(line);
+		(write(fd, line, ft_strlen(line)), free(line));
 	}
-
-	// Normal completion - restore IO and signal handler
-	restore_io(in_backup, -1);
-	signal(SIGINT, handler);
-	return (0);
+	return (restore_io(in_backup, -1), signal(SIGINT, handler), 0);
 }
 
 char	*heredoc(t_cmd *cmd, int *index)
@@ -136,22 +98,8 @@ char	*heredoc(t_cmd *cmd, int *index)
 		clean_heredoc(fd, filepath, stdin_backup, NULL);
 		return (NULL);
 	}
-
-	int result = read_heredoc(fd, clean_delim, stdin_backup, filepath);
-	close(fd);
-
-	// Check if heredoc was interrupted
-	if (result == -1)
-	{
-		unlink(filepath);  // Clean up the temp file
-		if (g_var == 2)
-		{
-			// Print error message here if not already printed
-			our_perror("Error: Failed to process heredoc\n");
-			// DON'T reset g_var here - let main loop handle it and set exit code 130
-			return (NULL);
-		}
+	if (read_heredoc(fd, clean_delim, stdin_backup, filepath) == -1)
 		return (NULL);
-	}
+	close(fd);
 	return (our_strdup(cmd->gc, filepath));
 }
